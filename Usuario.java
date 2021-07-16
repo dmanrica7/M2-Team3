aa20accd13d7d125d5cdc308815daf603a3d23b3
DavidRiveraArtero <davidtoy222@gmail.com>
2021-07-16 10:13:26 +0200

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,17 +1,19 @@
 
 public class Usuario {
 	
-	private Integer Telefono;
+protected Integer Telefono;
 	
-	private String Nombre;
+	protected String Nombre;
 	
-	private String DNI;
+	protected String DNI;
 	
-	private String Sexo;
+	protected String Sexo;
 	
-	private String Email;
+	protected String Email;
 	
-	private String Cuenta;
+	protected String Cuenta;
+	
+	protected double salarios;
 	
 	
 	//VARIABLES
@@ -22,5 +24,157 @@ public class Usuario {
 		this.Sexo= "";
 		this.Email = "";
 		this.Cuenta = "";
+		this.salarios = 0;
 	}
+
+
+	
+
+
+	public Usuario(Integer telefono, String nombre, String dNI, String sexo, String email, String cuenta,
+			double salario) {
+		Telefono = telefono;
+		Nombre = nombre;
+		DNI = dNI;
+		Sexo = sexo;
+		Email = email;
+		Cuenta = cuenta;
+		this.salarios = salario;
+	}
+
+
+
+
+
+	public Usuario(Integer telefono, String dNI) {
+		Telefono = telefono;
+		DNI = dNI;
+	}
+	
+	public Integer Cobrar() {
+		return Telefono;
+		
+	}
+
+
+	@Override
+	public Integer cobrar(Integer cobrar) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+	public Integer getTelefono() {
+		return Telefono;
+	}
+
+
+
+
+
+	public void setTelefono(Integer telefono) {
+		Telefono = telefono;
+	}
+
+
+
+
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+
+
+
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+
+
+
+
+	public String getDNI() {
+		return DNI;
+	}
+
+
+
+
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+
+
+
+
+	public String getSexo() {
+		return Sexo;
+	}
+
+
+
+
+
+	public void setSexo(String sexo) {
+		Sexo = sexo;
+	}
+
+
+
+
+
+	public String getEmail() {
+		return Email;
+	}
+
+
+
+
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+
+
+
+
+	public String getCuenta() {
+		return Cuenta;
+	}
+
+
+
+
+
+	public void setCuenta(String cuenta) {
+		Cuenta = cuenta;
+	}
+
+
+
+
+
+	public double getSalario() {
+		return salarios;
+	}
+
+
+
+
+
+	public void setSalario(double salario) {
+		this.salarios = salario;
+	}
+
+	
+	
 }
