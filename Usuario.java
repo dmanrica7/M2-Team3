612d1bf27665490a78fd22bbf0cedffd487ef281
DavidRiveraArtero <davidtoy222@gmail.com>
2021-07-16 09:34:11 +0200

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -4,4 +4,23 @@ public class Usuario {
 	private Integer Telefono;
 	
 	private String Nombre;
+	
+	private String DNI;
+	
+	private String Sexo;
+	
+	private String Email;
+	
+	private String Cuenta;
+	
+	
+	//VARIABLES
+	public Usuario () {
+		this.Telefono = 0;
+		this.Nombre = "";
+		this.DNI = "";
+		this.Sexo= "";
+		this.Email = "";
+		this.Cuenta = "";
+	}
 }
