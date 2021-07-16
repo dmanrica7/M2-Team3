5f20307dcf6ee7317d73e822643cb82b782b0b9d
DavidRiveraArtero <davidtoy222@gmail.com>
2021-07-16 12:36:12 +0200

    Version1

---
+++ Junior.java
@@ -0,0 +1,24 @@
+
+public class Junior extends Employee{
+	
+	
+	
+	public Junior(int telefono, String dni) {
+		super(telefono, dni);
+	}
+	
+	public Junior(Integer telefono, String nombre, String dNI, char sexo, String email, String cuenta,double salarios, double salario) {
+		
+	}
+	
+	public void comprobarPrecio() {
+		this.sueldo_final=this.salarios*0.85;
+		if (this.sueldo_final > 1600) {
+			this.sueldo_final = 1600;
+		}else if (this.sueldo_final < 900) {
+			this.sueldo_final = 900;
+		}
+		
+	}
+
+}
